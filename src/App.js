65cd0bfabf0da1e83057a5150feb9fc76a168e0f
learnaws288 <142102664+learnaws288@users.noreply.github.com>
2023-09-29 12:37:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To Ashok IT...!!
+          Welcome To IT School...!!
         </p>
         <a
           className="App-link"
-          href="https://ashokitech.com/"
+          href="https://flipkart.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
